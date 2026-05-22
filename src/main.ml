4afe39a62694d4c6646875670043f58a78e67f1b
GouzGouz <doudou7680@orange.fr>
2026-05-22 20:47:09 +0200

    Added ft_error to print error, added error handling

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,9 @@
+let delete_newline str =
+  String.map (function '\n' -> ' ' | x -> x) str
+
+let print_error str =
+  Printf.printf "Ft_turing: Error: %s\n" (delete_newline str)
+
 
 let main () =
   if Parser.parse_option () = true then (
@@ -5,9 +11,13 @@ let main () =
   )
   else (
     let filename = Sys.argv.(1) in
-    let json = Yojson.Safe.from_file filename in
-    let machine = Parser.create_machine json in
-    Debug.print_machine machine
+    try
+      let json = Yojson.Safe.from_file filename in
+      let machine = Parser.create_machine json in
+      Debug.print_machine machine
+    with
+    | Sys_error msg  -> print_error msg;
+    | Yojson.Json_error msg-> print_error msg;
   ) 
 
 
